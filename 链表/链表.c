9558579befbde5f5ad9f62a8da943db5d74a1d47
Xu_no_ghost <l18181138587@163.com>
2020-08-15 12:09:15 +0800

    添加链表的遍历和搜索

--- 链表/链表.c
+++ 链表/链表.c
@@ -8,36 +8,80 @@
 //     struct _node *next;
 // } Node;
 
-int main(int argc, char const *argv[])
+typedef struct _list
+{
+    Node *head;
+} List;
+
+void add(List *pList, int number);
+
+void print(List *pList);
+
+    int main(int argc, char const *argv[])
 {
     Node *head = NULL;
+    List list;
+    list.head = NULL;
     int number;
     do
     {
         scanf("%d",&number);
         if( number != -1)
         {
-            //添加到链表
-            Node *p = (Node*)malloc(sizeof(Node));
-            p->value = number;
-            p->next = NULL;
-            //找到最后的指向
-            Node *last = head;
-            if( last )
-            {
-                while( last->next)
-                {
-                    last = last->next;
-                }
-                //链接
-                last->next = p;
-            }
-            else
-            {
-                head = p;
-            }
+            add(&list,number);
         }
     } while ( number != -1);
+
+    //遍历
+    print(&list);
     
+    //搜索
+    scanf("%d",&number);
+    Node *p;
+    int isFound = 0;
+    for( p=list.head;p;p->next)
+    {
+        if( p->value == number)
+        {
+            printf("找到了\n");
+            isFound = 1;
+            break;
+        }
+    }
+    if( !isFound )
+        printf("没找到\n");
     return 0;
+}
+
+void add(List* pList, int number)
+{
+    //添加到链表
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->value = number;
+    p->next = NULL;
+    //找到最后的指向
+    Node *last = pList->head;
+    if (last)
+    {
+        while (last->next)
+        {
+            last = last->next;
+        }
+        //链接
+        last->next = p;
+    }
+    else
+    {
+        pList->head = p;
+    }
+}
+
+void print(List *pList)
+{
+    Node *p;
+    for (p = pList->head; p; p = p->next)
+    {
+        printf("%d\n", p->value);
+    }
+    printf("\n");
 }
